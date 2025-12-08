15587548cf30b36bdbe39604a261142c8341cb3b
Lahav32355 <71841679+Lahav32355@users.noreply.github.com>
2025-12-08 17:49:07 +0200

    Enhance TypeClass with member and ancestor methods
    Added methods for member and ancestor checks in TypeClass.

--- src/types/TypeClass.java
+++ src/types/TypeClass.java
@@ -2,25 +2,47 @@ package types;
 
 public class TypeClass extends Type
 {
-	/*********************************************************************/
-	/* If this class does not extend a father class this should be null  */
-	/*********************************************************************/
-	public TypeClass father;
+    public TypeClass father;
+    public TypeClassVarDecList dataMembers;
+    
+    public TypeClass(TypeClass father, String name, TypeClassVarDecList dataMembers)
+    {
+        this.name = name;
+        this.father = father;
+        this.dataMembers = dataMembers;
+    }
 
-	/**************************************************/
-	/* Gather up all data members in one place        */
-	/* Note that data members coming from the AST are */
-	/* packed together with the class methods         */
-	/**************************************************/
-	public TypeList dataMembers;
-	
-	/****************/
-	/* CTROR(S) ... */
-	/****************/
-	public TypeClass(TypeClass father, String name, TypeList dataMembers)
-	{
-		this.name = name;
-		this.father = father;
-		this.dataMembers = dataMembers;
-	}
+    @Override
+    public boolean isClass() { return true; }
+
+    @Override
+    public <R> R accept(TypeVisitor<R> visitor){return visitor.visit(this);}
+
+    /** Check if this class is ancestor of other */
+    public boolean isAncestorOf(TypeClass other)
+    {
+        // TODO: implement
+        return false;
+    }
+
+    /** Find member in this class only */
+    public TypeClassVarDec findMember(String memberName)
+    {
+        // TODO: implement
+        return null;
+    }
+
+    /** Find member in this class OR parent classes */
+    public TypeClassVarDec findMemberInHierarchy(String memberName)
+    {
+        // TODO: implement
+        return null;
+    }
+
+    /** Find only fields (not methods) in hierarchy */
+    public TypeClassVarDec findFieldInHierarchy(String fieldName)
+    {
+        // TODO: implement
+        return null;
+    }
 }
