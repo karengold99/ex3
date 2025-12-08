ee04a926548534914fa80b105d506a8982936823
Lahav32355 <71841679+Lahav32355@users.noreply.github.com>
2025-12-08 15:46:18 +0200

    Implement TypeNil singleton class

---
+++ src/types/TypeNil.java
@@ -0,0 +1,36 @@
+package types;
+
+public class TypeNil extends Type
+{
+    /**************************************/
+    /* USUAL SINGLETON IMPLEMENTATION ... */
+    /**************************************/
+    private static TypeNil instance = null;
+
+    /*****************************/
+    /* PREVENT INSTANTIATION ... */
+    /*****************************/
+    protected TypeNil() {}
+
+    /******************************/
+    /* GET SINGLETON INSTANCE ... */
+    /******************************/
+    public static TypeNil getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new TypeNil();
+            instance.name = "nil";
+        }
+        return instance;
+    }
+
+    @Override
+    public boolean isNil() {return true;}
+
+    /*==================================================================*/
+    /*                    VISITOR PATTERN                               */
+    /*==================================================================*/
+	@Override
+    public <R> R accept(TypeVisitor<R> visitor) { return visitor.visit(this); }
+}
