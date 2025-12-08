94129bf94febc82ec19ba494ee74eaa344594810
Lahav32355 <71841679+Lahav32355@users.noreply.github.com>
2025-12-08 17:39:10 +0200

    Implement type utility methods in TypeUtils
    Added utility methods for type checks with placeholders.

--- src/types/TypeUtils.java
+++ src/types/TypeUtils.java
@@ -1,1 +1,32 @@
+package types;
 
+public class TypeUtils
+{
+    /** Check if value can be assigned to target */
+    public static boolean canAssignTo(Type value, Type target)
+    {
+        // TODO: implement using AssignmentChecker
+        return false;
+    }
+
+    /** Check if expr type can be returned from function */
+    public static boolean canReturn(Type exprType, Type returnType)
+    {
+        // TODO: implement
+        return false;
+    }
+
+    /** Check if child extends parent */
+    public static boolean isChild(Type child, Type parent)
+    {
+        // TODO: implement
+        return false;
+    }
+
+    /** Check if type is int or string */
+    public static boolean isIntOrString(Type t)
+    {
+        // TODO: implement
+        return false;
+    }
+}
