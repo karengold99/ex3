3a0a1249a62de3c7ae4aaac84ab39c40b51b2276
Lahav32355 <71841679+Lahav32355@users.noreply.github.com>
2025-12-08 17:50:27 +0200

    Implement member management methods in TypeClassVarDecList
    Added methods for finding, inheriting, and inserting members.

--- src/types/TypeClassVarDecList.java
+++ src/types/TypeClassVarDecList.java
@@ -2,12 +2,33 @@ package types;
 
 public class TypeClassVarDecList
 {
-	public TypeClassVarDec head;
-	public TypeClassVarDecList tail;
-	
-	public TypeClassVarDecList(TypeClassVarDec head, TypeClassVarDecList tail)
-	{
-		this.head = head;
-		this.tail = tail;
-	}	
+    public TypeClassVarDec head;
+    public TypeClassVarDecList tail;
+    
+    public TypeClassVarDecList(TypeClassVarDec head, TypeClassVarDecList tail)
+    {
+        this.head = head;
+        this.tail = tail;
+    }
+
+    /** Find member by name in this list */
+    public TypeClassVarDec find(String name)
+    {
+        // TODO: implement
+        return null;
+    }
+
+    /** Create inherited copy of all members */
+    public TypeClassVarDecList inherit()
+    {
+        // TODO: implement
+        return null;
+    }
+
+    /** Insert new member at end */
+    public TypeClassVarDecList insert(TypeClassVarDec d)
+    {
+        // TODO: implement
+        return null;
+    }
 }
