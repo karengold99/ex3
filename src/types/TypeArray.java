b9a24447326579b90aa03df255a2f158ccfb613b
Lahav32355 <71841679+Lahav32355@users.noreply.github.com>
2025-12-08 17:51:51 +0200

    Refactor TypeArray to extend Type class
    Refactored TypeArray to extend Type and updated constructor and methods accordingly.

--- src/types/TypeArray.java
+++ src/types/TypeArray.java
@@ -1,35 +1,21 @@
 package types;
 
-public class TypeArray {
-    private String elementType;
-    private int size;
+public class TypeArray extends Type
+{
+    public Type elementType;
 
-    public TypeArray(String elementType, int size) {
+    public TypeArray(String name, Type elementType)
+    {
+        this.name = name;
         this.elementType = elementType;
-        this.size = size;
-    }
-
-    public String getElementType() {
-        return elementType;
-    }
-
-    public void setElementType(String elementType) {
-        this.elementType = elementType;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
     }
 
     @Override
-    public String toString() {
-        return "TypeArray{" +
-                "elementType='" + elementType + '\'' +
-                ", size=" + size +
-                '}';
+    public boolean isArray() { return true; }
+
+    @Override
+    public <R> R accept(TypeVisitor<R> visitor)
+    {
+        return visitor.visit(this);
     }
 }
