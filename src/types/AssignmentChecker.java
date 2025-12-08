22f687ad4b8221ce0fc7a08f0e91c33df96a9a35
Lahav32355 <71841679+Lahav32355@users.noreply.github.com>
2025-12-08 17:43:21 +0200

    Add AssignmentChecker for type assignment checks
    Implemented the AssignmentChecker class to evaluate type assignments.

--- src/types/AssignmentChecker.java
+++ src/types/AssignmentChecker.java
@@ -1,1 +1,64 @@
+package types;
 
+/**
+ * Visitor that checks if a value can be assigned to a target type.
+ * Usage: targetType.accept(new AssignmentChecker(valueType))
+ */
+public class AssignmentChecker implements TypeVisitor<Boolean>
+{
+    private Type valueType;
+    
+    public AssignmentChecker(Type valueType)
+    {
+        this.valueType = valueType;
+    }
+
+    @Override
+    public Boolean visit(TypeInt type)
+    {
+        // TODO:
+        return false;
+    }
+
+    @Override
+    public Boolean visit(TypeString type)
+    {
+        // TODO:
+        return false;
+    }
+
+    @Override
+    public Boolean visit(TypeVoid type)
+    {
+        // TODO:
+        return false;
+    }
+
+    @Override
+    public Boolean visit(TypeNil type)
+    {
+        // TODO:
+        return false;
+    }
+
+    @Override
+    public Boolean visit(TypeClass type)
+    {
+        // TODO:
+        return false;
+    }
+
+    @Override
+    public Boolean visit(TypeArray type)
+    {
+        // TODO:
+        return false;
+    }
+
+    @Override
+    public Boolean visit(TypeFunction type)
+    {
+        // TODO:
+        return false;
+    }
+}
