9f53fed56a2b7be0cb60d01fa13420927224d0f0
Lahav32355 <71841679+Lahav32355@users.noreply.github.com>
2025-12-08 17:46:10 +0200

    Implement accept method in TypeForScopeBoundaries

--- src/types/TypeForScopeBoundaries.java
+++ src/types/TypeForScopeBoundaries.java
@@ -9,4 +9,10 @@ public class TypeForScopeBoundaries extends Type
 	{
 		this.name = name;
 	}
+	
+	@Override
+    public <R> R accept(TypeVisitor<R> visitor)
+    {
+        throw new RuntimeException("Cannot visit SCOPE-BOUNDARY type");
+    }
 }
