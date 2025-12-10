408db9e072c7560bc6fe60b66939901a8b4a7355
Lahav32355 <71841679+Lahav32355@users.noreply.github.com>
2025-12-10 19:13:36 +0200

    Update AstExpBinop.java

--- src/ast/AstExpBinop.java
+++ src/ast/AstExpBinop.java
@@ -1,10 +1,12 @@
 package ast;
 
 import types.*;
+import semantic.SemanticException;
 
 public class AstExpBinop extends AstExp
 {
-	int op;
+	// op: 0=PLUS, 1=MINUS, 2=TIMES, 3=DIVIDE, 4=LT, 5=GT, 6=EQ
+	public int op;
 	public AstExp left;
 	public AstExp right;
 	
@@ -43,7 +45,11 @@ public class AstExpBinop extends AstExp
 		/*********************************/
 		if (op == 0) {sop = "+";}
 		if (op == 1) {sop = "-";}
-		if (op == 3) {sop = "=";}
+		if (op == 2) {sop = "*";}
+		if (op == 3) {sop = "/";}
+		if (op == 4) {sop = "<";}
+		if (op == 5) {sop = ">";}
+		if (op == 6) {sop = "=";}
 
 		/**********************************/
 		/* AST NODE TYPE = AST BINOP EXP */
@@ -71,20 +77,53 @@ public class AstExpBinop extends AstExp
 		if (right != null) AstGraphviz.getInstance().logEdge(serialNumber,right.serialNumber);
 	}
 
-	public Type semantMe()
+	@Override
+	public Type semantMe() throws SemanticException
 	{
-		Type t1 = null;
-		Type t2 = null;
+		Type t1 = left.semantMe();
+		Type t2 = right.semantMe();
 		
-		if (left  != null) t1 = left.semantMe();
-		if (right != null) t2 = right.semantMe();
+		// op: 0=PLUS, 1=MINUS, 2=TIMES, 3=DIVIDE, 4=LT, 5=GT, 6=EQ
 		
-		if ((t1 == TypeInt.getInstance()) && (t2 == TypeInt.getInstance()))
+		if (op == 0) // PLUS
 		{
+			// PDF 2.6: + works on int+int or string+string
+			if (t1.isInt() && t2.isInt())
+				return TypeInt.getInstance();
+			if (t1.isString() && t2.isString())
+				return TypeString.getInstance();
+			throw new SemanticException(lineNumber, "'+' requires two ints or two strings");
+		}
+		else if (op >= 1 && op <= 3) // MINUS, TIMES, DIVIDE
+		{
+			// PDF 2.6: -, *, / only work on int
+			if (!t1.isInt() || !t2.isInt())
+				throw new SemanticException(lineNumber, "arithmetic operator requires int operands");
+			
+			// PDF 2.6: Division by constant 0 is error
+			if (op == 3 && right instanceof AstExpInt) {
+				int val = ((AstExpInt) right).value;
+				if (val == 0)
+					throw new SemanticException(lineNumber, "division by zero");
+			}
 			return TypeInt.getInstance();
 		}
-		System.exit(0);
-		return null;
+		else if (op == 4 || op == 5) // LT, GT
+		{
+			// PDF 2.6: <, > only work on int
+			if (!t1.isInt() || !t2.isInt())
+				throw new SemanticException(lineNumber, "comparison operator requires int operands");
+			return TypeInt.getInstance();
+		}
+		else if (op == 6) // EQ
+		{
+			// PDF 2.6: = equality testing
+			if (!TypeUtils.canCompareEquality(t1, t2))
+				throw new SemanticException(lineNumber, "cannot compare these types for equality");
+			return TypeInt.getInstance();
+		}
+		
+		throw new SemanticException(lineNumber, "unknown binary operator");
 	}
 
 }
