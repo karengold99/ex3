6a79f58ce71050fb86ad27c683cc3afdaddbe800
Lahav32355 <71841679+Lahav32355@users.noreply.github.com>
2025-12-10 17:27:29 +0200

    Update TypeInt.java

--- src/types/TypeInt.java
+++ src/types/TypeInt.java
@@ -25,9 +25,5 @@ public class TypeInt extends Type
 	}
 	@Override
 	public boolean isInt() { return true; }
-	/*==================================================================*/
-    /*                    VISITOR PATTERN                               */
-    /*==================================================================*/
-	@Override
-    public <R> R accept(TypeVisitor<R> visitor) { return visitor.visit(this); }
+
 }
